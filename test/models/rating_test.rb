5f483e53ce3ea07761581bd8a48f7dbda9070d15
PauloBommfim <paulobommfim@gmail.com>
2019-09-20 14:51:48 -0300

    Define test validations to NPS

--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class RatingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "valid if score is between 0 and 10" do
+    0.upto(10).each do |i|
+      rating = Rating.new(score: i)
+      rating.valid?
+      assert_empty rating.errors[:score]
+    end
+  end
+
+  test "invalid if score is less than 0" do
+    rating = Rating.new(socre: -1)
+    rating.valid?
+    assert_not rating.errors[:score].empty?
+  end
+
+  test "invalid if score is treater than 10" do
+    rating = Rating.new(score: 11)
+    rating.valid?
+    assert_not rating.errors[:score].empty?
+  end
 end
